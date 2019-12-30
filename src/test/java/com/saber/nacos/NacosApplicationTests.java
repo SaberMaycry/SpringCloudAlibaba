9f554b26835e711b30b18dfeb2441ee39212e4d9
SaberXu <xushibo@annetinfo.com>
2019-12-30 17:47:57 +0800

    初始化构建项目，提交。

---
+++ src/test/java/com/saber/nacos/NacosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saber.nacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
